46eae0d150eea83f0d85a3880ac78446ddd10f38
Bossa Web Solutions <gvfbossa@hotmail.com>
2025-05-26 13:43:58 -0300

    docsrm

--- src/app/services/sheet.service.ts
+++ src/app/services/sheet.service.ts
@@ -15,7 +15,7 @@ export class SheetService {
   }
 
   deleteSheetByName(nome: string): Observable<void> {
-  return this.http.delete<void>(`${this.baseUrl}api/sheets/${encodeURIComponent(nome)}`);
+  return this.http.delete<void>(`${this.baseUrl}api/sheets/${encodeURIComponent('MARGEM CARTAO CAPITAL - ' + nome + '.csv')}`);
 }
 
   
