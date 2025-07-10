25fb89722dba8e5019c4ecccc92d69568ae3412e
Bossa Web Solutions <gvfbossa@hotmail.com>
2025-07-10 14:17:45 -0300

    refresh da pagina

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  ]
 };
